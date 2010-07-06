b312187a299dfa65360b17429ab118c405fb4122
Matt Soucy <demosthenes2k7@gmail.com>
2010-07-06 11:57:30 -0400

    Added tasks.h, I forgot to commit that one.

---
+++ framework166/Tasks.h
@@ -0,0 +1,5 @@
+// This doesn't get include guards, it's only used once.
+// Declare your task objects here
+// Example:
+// DriveClass DriveObject;
+// No need for pointers, or anything else, if it's derived from Team166Task
